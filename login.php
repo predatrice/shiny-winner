120adb941dc9dd82c4f91faaa633af4e410db61d
Chirin <predatrice@gmail.com>
2017-07-31 04:18:49 +0000

    update login

---
+++ login.php
@@ -0,0 +1,87 @@
+<?php
+//get database connection
+include("includes/database.php");
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $user_email = $_POST["user"];
+    
+    //check if the user enrered and email address
+    if(filter_var($user_email,FILTER_VALIDATE_EMAIL)){
+        //if true, user entered an email
+        $query = "SELECT * FROM accounts WHERE email='$user_email'";
+    }
+    else{
+        //if false, user entered an email
+        $query = "SELECT * FROM accounts WHERE username='$user_email'";
+    }
+    $password = $_POST["password"];
+    
+    //construct query with email variable
+    //$query = "SELECT * FROM accounts WHERE email='$email'";
+    
+    //create array to store errors
+    $errors = array();
+    
+    //run query
+    $userdata = $connection->query($query);
+    
+    //check the result
+    if($userdata->num_rows > 0){
+        $user = $userdata->fetch_assoc();
+        if(password_verify($password,$user["password"])===false){
+            $errors["account"] = "email or password incorrect";
+        }
+        else{
+            $message = "You have been  logged in";
+        }
+    }
+    else{
+        $errors["account"] = "there is no user with the supplied credentials";
+    }
+}
+?>
+<!doctype html>
+<html>
+    <?php
+    $page_title = "Login to your Account";
+    include("includes/head.php");
+    ?>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <form id="login-form" action="login.php" method="post">
+                    <h1>Login to your account</h1>
+                    <div class="form-group">
+                        <label for="user">Email Address or Username</label>
+                        <input class="form-control" type="text" id="user" name="user" placeholder="you@email.com or username">
+                    </div>
+                    <div class="form-group">
+                        <label for="password"> Your Password</label>
+                        <input class="form-control" type="password" id="password" name="password" placeholder="your password">
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" name="submit" value="login" class="btn btn-info">Login</button>
+                     </div>
+                    </form>
+                    <?php
+                    if(count($errors) > 0 || $message){
+                        //see Whicht class to be used with alert
+                        if(count($errors) > 0){
+                            $class="alert-warning";
+                            $feedback = implode(" ",$errors);
+                        }
+                        if($message){
+                            $class="alert-success";
+                            $feedback = $message;
+                        }
+                        echo "<div class=\"alert $class\">
+                            $feedback
+                         </div>";
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
