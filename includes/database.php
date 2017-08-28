8c6315fc05d241a48863b28d80f1cd4f04164577
Chirin <predatrice@gmail.com>
2017-08-28 02:45:00 +0000

    updated accounts database

--- includes/database.php
+++ includes/database.php
@@ -1,8 +1,8 @@
 <?php
-$dbuser = "user";
-$dbpassword = "password";
-$dbhost = "localhost";
-$dbdatabase = "datastore";
+$dbhost = getenv("dbhost");
+$dbuser = getenv("dbuser");
+$dbpassword = getenv("dbpassword");
+$dbdatabase = getenv("dbname");
 $connection = mysqli_connect($dbhost,$dbuser,$dbpassword,$dbdatabase);
 if(!$connection){
   echo "database error";
